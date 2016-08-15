196a27708738b11b9abd036746ce9f47812428d2
pTricKg <ptrickg@hotmail.com>
2016-08-14 20:57:27 -0600

    first c# - hello world
    hello world

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Big, Bad World!");
+
+            String user = Console.ReadLine();
+
+            Console.WriteLine(user + ", you say!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
