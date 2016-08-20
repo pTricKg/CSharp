b3bddd0d3af1385e458dedbb1eff3a48ef856c80
pTricKg <ptrickg1976@gmail.com>
2016-08-19 18:11:30 -0600

    folder for game dev in C#

---
+++ GameC#/ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! This Application will calculate the distance between two points.");
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
