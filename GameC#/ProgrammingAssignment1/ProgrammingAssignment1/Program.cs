d684942ff2e634a455aa3195414f65a464572df2
pTricKg <ptrickg1976@gmail.com>
2016-08-21 15:32:10 -0600

    get input and keep window open and some notes for TODO

--- GameC#/ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
+++ GameC#/ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -14,11 +14,36 @@ namespace ProgrammingAssignment1
             Console.WriteLine("Welcome! This Application will calculate the distance between two points and the angle between those points.\n");
 
             // get user input and store
+            Console.WriteLine("Please input 2 x coordinate, press enter after each: ");
+
             float point1X = float.Parse(Console.ReadLine());
             float point2X = float.Parse(Console.ReadLine());
 
+            // print input
+            Console.WriteLine("You input " + point1X + " as X1.");
+            Console.WriteLine("You input " + point2X + " as X2.");
+
+            Console.WriteLine("Please input 2 y coordinates, press enter after each: ");
             float point1Y = float.Parse(Console.ReadLine());
             float point2Y = float.Parse(Console.ReadLine());
+
+            // print input
+            Console.WriteLine("You input " + point1Y + " as Y1.");
+            Console.WriteLine("You input " + point2Y + " as Y2.");
+
+            // calcualte delta x and delta y
+
+            // calculate distance between two points(Pythagorean Theorem)
+
+            // calculate angle to go from point 1 to point 2(Atan2 math class)
+            // convert from radians to degrees
+
+            // print output to 3 decimal places for distance
+            // print output of angle
+
+            // keep window
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
         }
     }
 }
