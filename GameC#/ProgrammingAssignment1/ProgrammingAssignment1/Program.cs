c1a9e3c5f6d06829ee533293959da62a253021c8
pTricKg <ptrickg1976@gmail.com>
2016-08-21 15:06:36 -0600

    init project

---
+++ GameC#/ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print to console
+            Console.WriteLine("Welcome! This Application will calculate the distance between two points and the angle between those points.\n");
+
+            // get user input and store
+            float point1X = float.Parse(Console.ReadLine());
+            float point2X = float.Parse(Console.ReadLine());
+
+            float point1Y = float.Parse(Console.ReadLine());
+            float point2Y = float.Parse(Console.ReadLine());
+        }
+    }
+}
