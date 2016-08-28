2cdf1de9d97729e7c9793dc8a732758359380603
pTricKg <ptrickg1976@gmail.com>
2016-08-28 17:41:10 -0600

    Temp Conversion Fun

---
+++ GameC#/TempConversion/TempConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get Fahrenheit from user input
+            Console.WriteLine("Please enter Fahrenheit temp for conversion to Celsius: ");
+            double userFahrenheit = Console.ReadLine();
+
+            // Conversion for F to C degrees: F - 32 / 9 * 5
+            // Conversion for C to F: C * 9 / 5 + 32
+        }
+    }
+}
