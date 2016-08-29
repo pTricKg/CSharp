a7068c6a822e3c1098e054804ae9e45afc73a988
pTricKg <ptrickg1976@gmail.com>
2016-08-29 15:16:25 -0600

    all sprites populate, had typo caused crash

--- GameC#/ProgrammingAssignment2/ProgrammingAssignment2/Game1.cs
+++ GameC#/ProgrammingAssignment2/ProgrammingAssignment2/Game1.cs
@@ -73,13 +73,13 @@ namespace ProgrammingAssignment2
 
             // STUDENTS: load the sprite images here
             droid0 = Content.Load<Texture2D>(@"graphics\android0");
-            drawRectangle0 = new Rectangle(150, 100, droid0.Width, droid0.Height);
+            drawRectangle0 = new Rectangle(150, 100, WindowWidth, WindowHeight);
 
-            //droid1 = Content.Load<Texture2D>(@"graphics\andriod1");
-            //drawRectangle1 = new Rectangle(300, 200, droid1.Width, droid1.Height);
+            droid1 = Content.Load<Texture2D>(@"graphics\android1");
+            drawRectangle1 = new Rectangle(300, 200, WindowWidth, WindowHeight);
 
             droid2 = Content.Load<Texture2D>(@"graphics\android2");
-            drawRectangle2 = new Rectangle(400, 300, droid2.Width, droid2.Height);
+            drawRectangle2 = new Rectangle(400, 300, WindowWidth, WindowHeight);
 
             // STUDENTS: set the currentSprite variable to one of your sprite variables
 
@@ -161,7 +161,7 @@ namespace ProgrammingAssignment2
             spriteBatch.Begin();
 
             spriteBatch.Draw(droid0, drawRectangle0, Color.White);
-            //spriteBatch.Draw(droid1, drawRectangle1, Color.White);
+            spriteBatch.Draw(droid1, drawRectangle1, Color.White);
             spriteBatch.Draw(droid2, drawRectangle2, Color.White);
 
             spriteBatch.End();
